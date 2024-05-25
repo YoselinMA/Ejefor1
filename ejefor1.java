5dc287f810a1c07918c9fcadf04489679795e655
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-24 22:40:59 -0600

    Create ejefor1.java

---
+++ ejefor1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejefor1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero, i, total=0;
+        System.out.println("Ingrese un numero: ");
+       numero= sc.nextInt();
+
+    
+        for(i=1; i<=numero; i++){
+        total +=i;
+        }
+        
+        System.out.println( total);
+    
+    }
+    
+}
